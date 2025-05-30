5e246d4b8fee67ab294ca70072416af8b988d097
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2025-05-30 23:24:48 +0100

    added sample unitests

---
+++ test/Sample.Tests/BasicEndpointTests.cs
@@ -0,0 +1,61 @@
+﻿using Sample.Logging.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Tests
+{
+    public class BasicEndpointTests : CustomWebApplicationFactory
+    {
+    public class BasicEndpointTests : CustomWebApplicationFactory
+
+    [Test]
+    public async Task Get_EndpointsReturnSuccessAndCorrectContentType()
+        {
+            string url = "/";
+            string contentType = "text/plain";
+
+            // Arrange
+            var client = this.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+            var correlationId = Guid.NewGuid().ToString();
+            requestMessage.Headers.Add(LoggingConstants.CorrelationId, correlationId);
+            var response = await client.SendAsync(requestMessage);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.That(contentType == response.Content.Headers.ContentType.MediaType);
+        }
+
+     [Test]
+    public async Task Get_Metadata()
+        {
+            string url = "/metadata";
+            string tmpLogicalCallContext = "xUserId=Test1|Machine=ABC";
+
+            // Arrange
+            var client = this.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
+            var correlationId = Guid.NewGuid().ToString();
+            requestMessage.Headers.Add(LoggingConstants.CorrelationId, correlationId);
+            requestMessage.Headers.Add(LoggingConstants.LogicalCallContext, tmpLogicalCallContex
+    
+            var response = await client.SendAsync(requestMessage);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.That(content.Contains(correlationId));
+            Assert.That(content.Contains(tmpLogicalCallContext));
+        }
+    }
+}
---
+++ test/Sample.Tests/LogicalCallContextHandlerTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Sample.Logging.Library;
+using Sample.Logging.Library.Middlewares;
+
+namespace Sample.Tests
+{
+    public class LogicalCallContextHandlerTests
+    {
+        private LogicalCallContextBuilderMiddleware middleware;
+        private RequestDelegate next = async (ctx) =>
+        {
+            await ctx.Response.WriteAsync("Test");
+            await Task.CompletedTask;
+        };
+
+        private Mock<ILogger<LogicalCallContextBuilderMiddleware>> mockLogger;
+
+        public LogicalCallContextHandlerTests()
+        {
+            mockLogger = new Mock<ILogger<LogicalCallContextBuilderMiddleware>>();
+            middleware = new LogicalCallContextBuilderMiddleware(next, mockLogger.Object);
+        }
+
+        [Test]
+        public async Task AddLogicalCallContextIntoHttpClient()
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "http://localhost");
+
+            middleware = new LogicalCallContextBuilderMiddleware(async (ctx) =>
+            {
+                using (var logicalCallContextHandler = new LogicalCallContextHandler(new TestHttpMessageHandler()))
+                {
+                    var httpClient = new HttpClient(logicalCallContextHandler);
+                    var responseMessage = await httpClient.SendAsync(requestMessage);
+                    await ctx.Response.WriteAsync("Test");
+                }
+            }, mockLogger.Object);
+
+            var correlationId = Guid.NewGuid().ToString();
+            var context = new DefaultHttpContext();
+            context.Connection.RemoteIpAddress = new System.Net.IPAddress(new byte[] { 1, 1, 1, 1 });
+            context.Request.Headers.TryAdd(LoggingConstants.CorrelationId, correlationId);
+            using (context.Response.Body = new MemoryStream())
+            {
+                await middleware.Invoke(context);
+            }
+
+            requestMessage.Headers.Should().NotBeNull();
+            requestMessage.Headers.GetValues(LoggingConstants.CorrelationId).First().Should().Be(correlationId);
+            requestMessage.Headers.GetValues(LoggingConstants.LogicalCallContext).First().Should().NotBeNullOrEmpty();
+        }
+
+        class TestHttpMessageHandler : HttpMessageHandler
+        {
+            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancell
+            {
+                return new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            }
+        }
+    }
+}
