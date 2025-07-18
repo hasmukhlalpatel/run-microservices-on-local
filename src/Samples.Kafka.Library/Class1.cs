40c292d59f04bf4a2785eecac6b2231e11e3abc7
Hasmukh Patel <hasmukhlalpatel@live.co.uk>
2025-07-18 19:26:49 +0100

    Kafka Schema Generator

---
+++ src/Samples.Kafka.Library/Class1.cs
@@ -0,0 +1,19 @@
+﻿using NJsonSchema.NewtonsoftJson.Generation;
+
+namespace Samples.Kafka.Library
+{
+    public class SchemaGenerator
+    {
+
+        public static string GenerateSchema<T>() => NewtonsoftJsonSchemaGenerator.FromType<T>(new NewtonsoftJsonSchemaGeneratorSettings
+        {
+            SerializerSettings = new Newtonsoft.Json.JsonSerializerSettings
+            {
+                TypeNameHandling = Newtonsoft.Json.TypeNameHandling.Auto,
+                Formatting = Newtonsoft.Json.Formatting.Indented,
+                //Converters = [new Samples.Serialization.Library.PolymorphicJsonConverter<T>()]
+
+            }
+        }).ToJson();
+    }
+}
